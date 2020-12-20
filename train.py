99dddd0f4cfec69ad4db6b03e69684b0882fc492
mdubouch <m.dubouchet18@imperial.ac.uk>
2020-12-20 14:44:27 +0100

    Logging to file

--- train.py
+++ train.py
@@ -1,11 +1,16 @@
 import argparse
+import logging
+
 
 parser = argparse.ArgumentParser('Train CDC GAN')
 parser.add_argument('--ngf', type=int, default=16)
 parser.add_argument('--ndf', type=int, default=16)
 parser.add_argument('--latent-dims', type=int, default=256)
 parser.add_argument('--sequence-length', type=int, default=2048)
+parser.add_argument('--log', type=str, default='info')
 args = parser.parse_args()
+
+logging.basicConfig(filename='output.log', level=getattr(logging, args.log.upper()), format='%(asctime)s %(message)s')
 
 ngf = args.ngf
 ndf = args.ndf
@@ -15,19 +20,19 @@ seq_len = args.sequence_length
 print('Importing networks...')
 import networks
 gen = networks.Gen(ngf=ngf, latent_dims=latent_dims, seq_len=seq_len)
-print(gen)
+logging.info(gen)
 disc = networks.Disc(ndf=ndf, seq_len=seq_len)
-print(disc)
+logging.info(disc)
 
 print('Importing geometry...')
 import geom_util as gu
-print('cumulative wires', gu.cum_n_wires)
+logging.info('cumulative wires {0}'.format(gu.cum_n_wires))
 
 print('Importing dataset...')
 import data
-print('pot', data.n_pot, ' bunches', data.n_bunches)
-print('dtypes', data.tree.dtype)
-print('shape', data.tree.shape)
+logging.info('pot %d  bunches %d', data.n_pot, data.n_bunches)
+logging.info('dtypes {0}'.format(data.tree.dtype))
+logging.info('shape {0}'.format(data.tree.shape))
 
 print('Pre-processing...')
 
