4e5b833a55a24b8b798fecff2d48c024da980b90
mdubouch <m.dubouchet18@imperial.ac.uk>
2021-06-24 14:50:56 +0200

    1

--- cdc_embedding.py
+++ cdc_embedding.py
@@ -5,12 +5,8 @@ import torch.nn.functional as F
 class CDCEmbedding(nn.Module):
     def __init__(self, context_size, hidden_size, n_wires):
         super().__init__()
-        self.emb = nn.Embedding(n_wires, hidden_size, sparse=False)
-        self.lin = nn.Sequential(
-                nn.Linear(hidden_size, 128),
-                nn.ReLU(),
-                nn.Linear(128, n_wires)
-        )
+        self.emb = nn.Embedding(n_wires, hidden_size)
+        self.lin = nn.Linear(hidden_size, n_wires)
     def forward(self, x):
         # shape=(batch, context_size)
         # Embed the neighbors
